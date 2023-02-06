23a2f3461428ea75ac7b2c30eba5fb7412a5f06e
KosolapovR <rkosolapov@jtconsulting.ru>
2023-02-06 10:18:15 +0100

    v 1.2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import axios, { AxiosError } from 'axios';
 import { MakeGenerics, ReactLocation } from '@tanstack/react-location';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useQueryClient } from '@tanstack/react-query';
 import {
   AuthTokenContext,
   IAuthTokenContext,
@@ -18,18 +18,14 @@ type LocationGenerics = MakeGenerics<{
 }>;
 
 const location = new ReactLocation<LocationGenerics>();
-const queryClient = new QueryClient();
-
-const production = process.env.NODE_ENV !== 'production';
 
 const App = () => {
-  // eslint-disable-next-line
-  const [_, forceRerender] = useState({});
   const [token, insertToken] = useState<string | null>(
     localStorage.getItem(API_TOKEN) !== 'null'
       ? localStorage.getItem(API_TOKEN)
       : null
   );
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     localStorage.setItem(API_TOKEN, token);
@@ -43,7 +39,6 @@ const App = () => {
           queryFn: me,
         });
         queryClient.setQueryData(queryKey, () => user);
-        forceRerender({});
       } catch (error) {
         queryClient.resetQueries({ queryKey, exact: true });
       }
@@ -77,24 +72,22 @@ const App = () => {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthTokenContext.Provider value={authContextValue}>
-        <AppRouter token={token} location={location} />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-        {!production && <ReactQueryDevtools initialIsOpen={false} />}
-      </AuthTokenContext.Provider>
-    </QueryClientProvider>
+    <AuthTokenContext.Provider value={authContextValue}>
+      <AppRouter token={token} location={location} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </AuthTokenContext.Provider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+ReactDOM.render(
+  <PersistQueryClientProvider
+    client={queryClient}
+    persistOptions={{ persister }}
+  >
+    <App />
+  </PersistQueryClientProvider>,
+  document.getElementById('root')
+);
